3bd05238f42e01dbd95a9fe3b7e80f2b579e79bc
Nicolás Salvo <nicosalvo@gmail.com>
2024-09-02 04:37:02 +0000

    feat: add dsl for tests

---
+++ models/tests/dsl/utils.go
@@ -0,0 +1,37 @@
+package dsl
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+func outputToString(callback func()) string {
+
+	// Create a pipe to capture the output
+	r, w, _ := os.Pipe()
+
+	// Save the original stdout
+	oldStdout := os.Stdout
+
+	// Assign the write end of the pipe to stdout
+	os.Stdout = w
+
+	// Ensure that stdout is restored after the test
+	defer func() {
+		os.Stdout = oldStdout
+		w.Close()
+	}()
+
+	// Call the function or code that prints to stdout
+	callback()
+
+	// Close the write end of the pipe to signal EOF
+	w.Close()
+
+	// Read the output from the read end of the pipe
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+
+	return buf.String()
+}
