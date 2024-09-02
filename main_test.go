fafdd144be0586bff78ab5aee3f7a9539011a7fc
Nicolás Salvo <nicosalvo@gmail.com>
2024-09-02 00:59:29 +0000

    test: add 2 expenses

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ import (
 func TestMain(t *testing.T) {
 	asserts := assert.New(t)
 
-	t.Run("should an expense with a description and amount", func(t *testing.T) {
+	t.Run("should add an expense with a description and amount", func(t *testing.T) {
 		// Given
 		amount := 20
 		description := "Lunch"
@@ -27,6 +27,26 @@ func TestMain(t *testing.T) {
 		asserts.Equal("Lunch", expenses[0].Description)
 		asserts.NotNil(expenses[0].CreatedAt)
 		asserts.Nil(expenses[0].UpdatedAt)
+	})
+
+	t.Run("should add two expenses", func(t *testing.T) {
+		// Given
+		amount := 20
+		description := "Lunch"
+		expenses := Expenses{}
+
+		expense1 := Expense{Amount: amount, Description: description}
+		expense2 := Expense{Amount: amount, Description: description}
+
+		// When
+		expenses.AddExpense(expense1)
+		expenses.AddExpense(expense2)
+
+		// Then
+		asserts.Equal(2, len(expenses))
+		asserts.Equal(1, expenses[0].Id)
+		asserts.Equal(2, expenses[1].Id)
+		asserts.True(expenses[0].CreatedAt.Before(expenses[1].CreatedAt))
 	})
 
 }
