8a42107779d67f4866232ee36ae0953bd687478e
Nicolás Salvo <nicosalvo@gmail.com>
2024-09-05 20:10:47 +0000

    refactor: test name

--- stores/tests/in-memory-store_test.go
+++ stores/tests/in-memory-store_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
+func TestInMemoryStore(t *testing.T) {
 	asserts := assert.New(t)
 
 	t.Run("✅ should instantiate an in-memory store", func(t *testing.T) {
